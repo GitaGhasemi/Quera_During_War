03522fb5d3e661a490892b76471a30facd341b9b
Gita Ghasemi <gita.ghasemi2003@gmail.com>
2025-06-23 13:17:55 +0330

    Adding two more questions while I still have access to Internet. :-)

---
+++ tavan2.py
@@ -0,0 +1,13 @@
+'''
+This is an answer to this quera question
+https://quera.org/problemset/616
+
+'''
+
+n = int(input())
+sum = 1
+
+while n >= sum:
+    sum = sum * 2
+
+print(sum)
---
+++ xor.py
@@ -0,0 +1,8 @@
+a = 3
+b = 5
+c = 5
+print(a ^ c ^ b)
+if (a ^ b):
+    print(0)
+else:
+    print(1)     
